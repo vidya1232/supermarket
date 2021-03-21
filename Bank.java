304d1afaa9b6756de5fa04143156ee6a7c03ead5
vidya <vidyamurugan1818gmail.com>
2021-03-21 12:39:57 +0530

    added only java file

---
+++ Bank.java
@@ -0,0 +1,22 @@
+class Bank
+{
+private int interestRate = 4; 
+  
+public int getInterestRate()
+{
+  return this.interestRate; 
+}
+public void setInterestRate(int val)
+{
+  if(val<4)
+  this.interestRate = val; 
+}
+  
+//int result = obj.add(5,10); 
+//System.out.println(c); 
+//public int add(int a, int b)
+{
+  //int c = a+b; 
+  //return c; 
+}
+}
